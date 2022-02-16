e8d68d33901f5eb996458939bd342acde3610e86
bin4xin <1904829268@qq.com>
2022-02-16 15:35:59 +0800

    [2022/02/16/15:35:21]<部署::生成配置文件代码-Action-1>commit by bin4xin.
    Signed-off-by: bin4xin <1904829268@qq.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+function _usage_(){
+cat << EOF
+Usage:
+     help|-h|--help             Displays this help
+     config <REMOTE_SERVER_IP>  Generate StrongPassword DIY to files.
+     password                   Generate StrongPassword
+
+Such as:
+    ./entrypoint.sh config 1.1.1.1
+    ./entrypoint.sh password
+    ./entrypoint.sh help|-h|--help
+EOF
+exit 0
+}
+
 function _Generate_Users_StrongPassword_Algorithm(){
 	local i tmp size max rand
 	# $RANDOM % (i+1) is biased because of the limited range of $RANDOM
@@ -12,30 +27,61 @@ function _Generate_Users_StrongPassword_Algorithm(){
 		rand=$(( rand % (i+1) ))
 		tmp=${Pass_Sting_Array[i]} Pass_Sting_Array[i]=${Pass_Sting_Array[rand]} Pass_Sting_Array[rand]=$tmp
 	done
-}
-
-function _Domain_MainAccess_Manage_ByAdmin(){
-	_Pass_From_GUSP_Algo=$(printf "%s" "${Pass_Sting_Array[@]}")
-	echo "plugin_passwd = $_Pass_From_GUSP_Algo" >> frpc.ini
-  # printf "%s" "${Pass_Sting_Array[@]}"
-	# frpc config file in root dir. next need to copy to every project.
+  _Pass_From_GUSP_Algo=$(printf "%s" "${Pass_Sting_Array[@]}")
 }
 
 function  _SCSMAIN_Domain_Mail_Manage_Logic(){
-	_Generate_Users_StrongPassword_Algorithm
-	_Domain_MainAccess_Manage_ByAdmin
-#	export Username=${Define_Mail_FullUsername}
-#	export Password=${_Pass_From_GUSP_Algo}
-#	envsubst  <./htmlTemp_Demo.html.template >./result.html
+_Generate_Users_StrongPassword_Algorithm
+# root dir or work dir. such as /annealing-plantform
+
+project=($(cat .github/workflows/deploy*.yml |grep "project:"| awk -F'"' '{print $2}'))
+project_name_CountPort=10010
+for porject_name in "${!project[@]}"; do
+let project_name_CountPort++
+
+cat >>${project[$porject_name]}.ini<<EOF
+#cat << EOF
+[common]
+server_addr = $REMOTE_SERVER_IP
+server_port = 7000
+token = B43niB43B43niB43niA5a
+tls_enable = true
+
+[项目_${project[$porject_name]}_SOCKS5_$project_name_CountPort]
+type = tcp
+remote_port = $project_name_CountPort
+plugin = socks5
+plugin_user = ${project[$porject_name]}
+plugin_passwd = $_Pass_From_GUSP_Algo
+use_compression = true
+EOF
+done
+mv ${project[$porject_name]}.ini ${project[$porject_name]}/
 }
 
-# Set gobal var here. Visit https://yandex.com/dev/domain/doc/reference/email-add.html to access api.
-#Aceess_EmailList_Manage_API="https://pddimp.yandex.ru/api2/admin/email"
-#Access_Domain="sentrylab.cn" #yournickname@yourdomain.com -> Access_Domain="yourdomain.com"
+if [ "$#" -eq 0 ] ; then
+   _usage_
+   exit 0
+fi
 
-# in action command is: bash SCS_domail_mail_manage.sh ${{ secrets.SENTRY_MAIL_PDDTOKEN }}
-#Define_PDD_Token=$1
-#Define_Mail_UsernameNosuffix=$2
-#Define_Mail_FullUsername="$Define_Mail_UsernameNosuffix@$Access_Domain"
+# some cases to check what function to transfer.
+case $1 in
+   config )
+       REMOTE_SERVER_IP=$2
+       _SCSMAIN_Domain_Mail_Manage_Logic
+       # ./scripts/entrypoint.sh config {{secrets.REMOTE_SERVER_IP}}
+       ;;
+   password )
+       _Generate_Users_StrongPassword_Algorithm
+       echo $_Pass_From_GUSP_Algo
+       # ./scripts/entrypoint.sh password
+       # to Generate StrongPassword
+       ;;
 
-_SCSMAIN_Domain_Mail_Manage_Logic
+   help | -h | --help )
+       _usage_
+       ;;
+   * )
+       printf "annealing-plantform：illegal option -- $1 \nTry '--help' for more information."
+       ;;
+esac
